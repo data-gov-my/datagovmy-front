5b02eaa85a59cd98068e3bff5457b8718c1d927a
heheheejin <e_jaxx@hotmail.com>
2023-03-10 15:23:00 +0800

    stye: fix responsive border in LeftRightCard

--- components/LeftRightCard/index.tsx
+++ components/LeftRightCard/index.tsx
@@ -16,7 +16,7 @@ const LeftRightCard: FunctionComponent<CardProps> = ({ left, right }) => {
   return (
     <>
       <div className="flex flex-col items-stretch overflow-hidden rounded-xl border border-slate-200 dark:border-zinc-800 lg:flex-row">
-        <div className="basis-1/3 border-r border-slate-200 dark:border-zinc-800 dark:bg-zinc-800/50">
+        <div className="basis-1/3 border-b border-slate-200 dark:border-zinc-800 dark:bg-zinc-800/50 lg:border-r">
           {left}
         </div>
         <div className="basis-2/3 bg-slate-50 dark:bg-zinc-900">{right}</div>
