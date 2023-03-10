f3842d031cd330f5954212cdbd25d822375cd308
heheheejin <e_jaxx@hotmail.com>
2023-03-10 12:02:40 +0800

    feat: create new card component

---
+++ components/LeftRightCard/index.tsx
@@ -0,0 +1,28 @@
+import { FunctionComponent, ReactNode } from "react";
+
+interface CardProps {
+  left: ReactNode;
+  right: ReactNode;
+}
+
+/**
+ * Left = description
+ * Right = interactive area
+ * @param param0
+ * @returns
+ */
+
+const LeftRightCard: FunctionComponent<CardProps> = ({ left, right }) => {
+  return (
+    <>
+      <div className="flex flex-col items-stretch rounded-xl border border-slate-200 dark:border-zinc-800 lg:flex-row">
+        <div className="basis-1/3 border-r border-slate-200 dark:border-zinc-800 dark:bg-zinc-800/50">
+          {left}
+        </div>
+        <div className="basis-2/3 overflow-hidden bg-slate-50 dark:bg-zinc-900">{right}</div>
+      </div>
+    </>
+  );
+};
+
+export default LeftRightCard;
