d4b17ba301e29cc63bc0159f6ac283ebe8dca1c7
irfanismail <irfan.ismail96@gmail.com>
2023-03-23 23:39:12 +0800

    fix: map osm wrap

--- components/OSMapWrapper/index.tsx
+++ components/OSMapWrapper/index.tsx
@@ -1,5 +1,5 @@
-import { FunctionComponent, MutableRefObject, ReactElement, useEffect, useRef } from "react";
-import { LatLng, LatLngBounds, LatLngExpression, LatLngTuple, Map } from "leaflet";
+import { FunctionComponent, ReactElement, useEffect } from "react";
+import { LatLng, LatLngBounds, LatLngExpression } from "leaflet";
 import { MapContainer, TileLayer, useMap, Marker, Popup, GeoJSON } from "react-leaflet";
 import type { GeoJsonObject } from "geojson";
 import bbox from "geojson-bbox";
@@ -7,8 +7,6 @@ import { useTheme } from "next-themes";
 
 type OSMapWrapperProps = {
   className?: string;
-  title?: string | ReactElement;
-  date?: string;
   position?: LatLngExpression;
   enableZoom?: boolean;
   zoom?: number;
@@ -22,9 +20,7 @@ type MarkerProp = {
 };
 
 const OSMapWrapper: FunctionComponent<OSMapWrapperProps> = ({
-  className = "h-full w-full",
-  title,
-  date,
+  className = "h-full lg:h-[500px] w-full",
   position = [5.1420589, 109.618149], // default - Malaysia
   enableZoom = true,
   zoom = 5,
@@ -34,47 +30,40 @@ const OSMapWrapper: FunctionComponent<OSMapWrapperProps> = ({
   const { theme } = useTheme();
 
   return (
-    <div className={className}>
-      {/* <div className="flex items-baseline justify-between pb-5">
-        <h4>{title}</h4>
-        {date && <p className="text-sm text-dim">{date}</p>}
-      </div> */}
-
-      <MapContainer
-        // className={["rounded-xl", className].join(" ")}
-        className={className}
-        center={position}
-        zoom={zoom}
-        zoomControl={enableZoom}
-        scrollWheelZoom={true}
-        minZoom={6}
-        maxBounds={new LatLngBounds(new LatLng(1, 97), new LatLng(10, 122))}
-        maxBoundsViscosity={1}
-      >
-        <OSMapControl position={position} zoom={zoom} geojson={geojson} />
-        {geojson && (
-          <GeoJSON
-            key={Math.random() * 10} // random uid required to refresh change in geojson
-            data={geojson}
-            style={{
-              color: "#000",
-              fill: false,
-              weight: 1,
-            }}
-          />
-        )}
-        <TileLayer
-          key={theme}
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url={`http://localhost:8080/styles/${theme}/{z}/{x}/{y}.png`}
+    <MapContainer
+      key={markers?.length} // random uid required to refresh change in geojson
+      className={className}
+      center={position}
+      zoom={zoom}
+      zoomControl={enableZoom}
+      scrollWheelZoom={true}
+      minZoom={6}
+      maxBounds={new LatLngBounds(new LatLng(1, 97), new LatLng(10, 122))}
+      maxBoundsViscosity={1}
+    >
+      <OSMapControl position={position} zoom={zoom} geojson={geojson} />
+      {geojson && (
+        <GeoJSON
+          key={Math.random() * 10} // random uid required to refresh change in geojson
+          data={geojson}
+          style={{
+            color: "#000",
+            fill: false,
+            weight: 1,
+          }}
         />
-        {markers?.map((item: any) => (
-          <Marker key={item.name} position={item.position} autoPan>
-            <Popup>{item.name}</Popup>
-          </Marker>
-        ))}
-      </MapContainer>
-    </div>
+      )}
+      <TileLayer
+        key={theme}
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url={`${process.env.NEXT_PUBLIC_TILESERVER_URL}/styles/${theme}/{z}/{x}/{y}.png`}
+      />
+      {markers?.map((item: any) => (
+        <Marker key={item.name} position={item.position} autoPan draggable>
+          <Popup>{item.name}</Popup>
+        </Marker>
+      ))}
+    </MapContainer>
   );
 };
 
