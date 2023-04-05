09eb73475b22bfa4bef11f2226d76d5f3247693d
leneherng <49067812+leneherng@users.noreply.github.com>
2023-04-05 19:08:27 +0800

    feat: initialise organ donation dashboard

--- pages/dashboard/organ-donation/index.tsx
+++ pages/dashboard/organ-donation/index.tsx
@@ -7,7 +7,13 @@ import Metadata from "@components/Metadata";
 import { useTranslation } from "@hooks/useTranslation";
 import OrganDonationDashboard from "@dashboards/healthcare/organ-donation";
 
-const OrganDonation: Page = ({}: InferGetStaticPropsType<typeof getStaticProps>) => {
+const OrganDonation: Page = ({
+  last_updated,
+  timeseries,
+  choropleth,
+  barchart_age,
+  barchart_time,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   const { t } = useTranslation(["common", "dashboard-organ-donation"]);
 
   return (
@@ -17,19 +23,30 @@ const OrganDonation: Page = ({}: InferGetStaticPropsType<typeof getStaticProps>)
         description={t("dashboard-organ-donation:description")}
         keywords={""}
       />
-      <OrganDonationDashboard />
+      <OrganDonationDashboard
+        last_updated={last_updated}
+        timeseries={timeseries}
+        choropleth={choropleth}
+        barchart_age={barchart_age}
+        barchart_time={barchart_time}
+      />
     </>
   );
 };
 // Disabled
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
   const i18n = await serverSideTranslations(locale!, ["common", "dashboard-organ-donation"]);
-  //   const { data } = await get("/dashboard", { dashboard: "currency" });
+  const { data } = await get("/dashboard", { dashboard: "organ_donation", state: "mys" });
 
   return {
     notFound: false,
     props: {
       ...i18n,
+      last_updated: new Date().valueOf(),
+      timeseries: data.timeseries,
+      choropleth: data.choropleth_malaysia,
+      barchart_age: data.barchart_age,
+      barchart_time: data.barchart_time,
     },
     revalidate: 60 * 60 * 24, // 1 day (in seconds)
   };
