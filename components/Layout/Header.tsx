870fb464ae0b547da5a8d118a57992f7003168e6
leneherng <49067812+leneherng@users.noreply.github.com>
2023-04-07 10:46:02 +0800

    fix: header theme switch, pad button

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -65,17 +65,11 @@ const Header: FunctionComponent<HeaderProps> = ({ stateSelector }) => {
               <NavItem title={"API Docs"} link="#" onClick={() => setIsTabletNavOpen(false)} />
               <NavItem title={"Articles"} link="#" onClick={() => setIsTabletNavOpen(false)} />
               <NavItem title={"Request Data"} link="#" onClick={() => setIsTabletNavOpen(false)} />
-
-              <div className="block md:hidden">
-                <ThemeToggle />
-              </div>
             </Nav>
           </div>
-          <div className="flex items-center gap-4">
+          <div className="flex items-center gap-3 md:gap-4">
             {stateSelector}
-            <div className="hidden md:block">
-              <ThemeToggle />
-            </div>
+            <ThemeToggle />
 
             {/* LANGUAGE DROPDOWN */}
             <Dropdown
@@ -86,12 +80,12 @@ const Header: FunctionComponent<HeaderProps> = ({ stateSelector }) => {
             {/* MOBILE NAV ICONS */}
             {isTabletNavOpen ? (
               <XMarkIcon
-                className="block h-5 w-5 text-black dark:text-white md:hidden"
+                className="-m-2 box-content block h-5 w-5 p-2 text-black dark:text-white md:hidden"
                 onClick={() => setIsTabletNavOpen(false)}
               />
             ) : (
               <Bars3BottomRightIcon
-                className="block h-5 w-5 text-black dark:text-white md:hidden"
+                className="-m-2 box-content block h-5 w-5 p-2 text-black dark:text-white md:hidden"
                 onClick={() => setIsTabletNavOpen(true)}
               />
             )}
@@ -118,7 +112,7 @@ const ThemeToggle = () => {
   return (
     <>
       <Button
-        className="group relative hidden overflow-hidden hover:bg-washed dark:hover:bg-washed-dark md:block"
+        className="group relative overflow-hidden hover:bg-washed dark:hover:bg-washed-dark"
         onClick={() => setTheme(theme === "light" ? "dark" : "light")}
       >
         <Transition
@@ -144,18 +138,6 @@ const ThemeToggle = () => {
           <SunIcon className="-m-0.5 h-5 w-5 text-dim dark:group-hover:text-white" />
         </Transition>
       </Button>
-
-      <div className="flex items-center justify-between gap-2 px-2 pt-1.5 text-sm font-medium md:hidden">
-        <p>{t("components.theme")}</p>
-
-        <Dropdown
-          width="w-fit"
-          onChange={_theme => setTheme(_theme?.value)}
-          placeholder={theme}
-          selected={options.find(_theme => _theme.value === theme)}
-          options={options}
-        />
-      </div>
     </>
   );
 };
