7f6f5932506907634135e8621b6b0ce49af40de1
irfanismail <irfan.ismail96@gmail.com>
2023-04-13 13:37:57 +0800

    fix: refactor header component

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -3,13 +3,10 @@ import Image from "next/image";
 import { FunctionComponent, ReactNode, useEffect, useState } from "react";
 import { useTranslation } from "@hooks/useTranslation";
 import { Bars3BottomRightIcon, XMarkIcon } from "@heroicons/react/24/solid";
-import { SunIcon, MoonIcon } from "@heroicons/react/20/solid";
 import { Transition } from "@headlessui/react";
-import { languages } from "@lib/options";
 import { useTheme } from "next-themes";
 import { useLanguage } from "@hooks/useLanguage";
 import Nav from "@components/Nav";
-import NavItem from "@components/Nav/Item";
 import Dropdown from "@components/Dropdown";
 import Container from "@components/Container";
 import Button from "@components/Button";
@@ -20,116 +17,48 @@ interface HeaderProps {
 
 const Header: FunctionComponent<HeaderProps> = ({ stateSelector }) => {
   const { t } = useTranslation("common");
-  const { language, onLanguageChange } = useLanguage();
 
   const [isTabletNavOpen, setIsTabletNavOpen] = useState(false);
 
   return (
     <div className="fixed top-0 left-0 z-30 w-full border-b dark:border-washed-dark">
       <Container background="bg-white dark:bg-black" className="flex items-center gap-4 py-[11px]">
-        <div className="flex w-full items-center justify-between">
-          <div className="flex items-center gap-4">
-            <Link href="/">
-              <div className="flex cursor-pointer gap-2">
-                <div className="flex w-8 items-center justify-center">
-                  <Image
-                    src="/static/images/logo.png"
-                    width={48}
-                    height={36}
-                    alt="datagovmy_logo"
-                  />
-                </div>
-                <h4>data.gov.my</h4>
+        <div className="flex w-full items-center gap-4">
+          <Link href="/">
+            <div className="flex cursor-pointer gap-2">
+              <div className="flex w-8 items-center justify-center">
+                <Image src="/static/images/logo.png" width={48} height={36} alt="datagovmy_logo" />
               </div>
-            </Link>
-            <Nav isTabletNavOpen={isTabletNavOpen}>
-              <NavItem
-                key={"/"}
-                title={t("nav.home")}
-                link="/"
-                onClick={() => setIsTabletNavOpen(false)}
-              />
-              <NavItem
-                title={t("nav.dashboards")}
-                link="/dashboard"
-                key="/dashboard"
-                onClick={() => setIsTabletNavOpen(false)}
-              />
-              <NavItem
-                title={t("nav.catalogue")}
-                key="/data-catalogue"
-                link="/data-catalogue"
-                onClick={() => setIsTabletNavOpen(false)}
-              />
+              <h4>data.gov.my</h4>
+            </div>
+          </Link>
 
-              <NavItem title={"API Docs"} link="#" onClick={() => setIsTabletNavOpen(false)} />
-              <NavItem title={"Articles"} link="#" onClick={() => setIsTabletNavOpen(false)} />
-              <NavItem title={"Request Data"} link="#" onClick={() => setIsTabletNavOpen(false)} />
-            </Nav>
-          </div>
-          <div className="flex items-center gap-3 lg:gap-4">
-            {stateSelector}
-            <ThemeToggle />
+          <Nav stateSelector={stateSelector}>
+            {close => (
+              <>
+                <Nav.Item key={"/"} title={t("nav.home")} link="/" onClick={close} />
+                <Nav.Item
+                  title={t("nav.dashboards")}
+                  link="/dashboard"
+                  key="/dashboard"
+                  onClick={close}
+                />
+                <Nav.Item
+                  title={t("nav.catalogue")}
+                  key="/data-catalogue"
+                  link="/data-catalogue"
+                  onClick={close}
+                />
 
-            {/* LANGUAGE DROPDOWN */}
-            <Dropdown
-              width="w-fit"
-              selected={languages.find(lang => lang.value === language)}
-              onChange={onLanguageChange}
-              options={languages}
-            />
-            {/* MOBILE NAV ICONS */}
-            {isTabletNavOpen ? (
-              <XMarkIcon
-                className="box-content block h-5 w-5 text-black dark:text-white lg:hidden"
-                onClick={() => setIsTabletNavOpen(false)}
-              />
-            ) : (
-              <Bars3BottomRightIcon
-                className="box-content block h-5 w-5 text-black dark:text-white lg:hidden"
-                onClick={() => setIsTabletNavOpen(true)}
-              />
+                <Nav.Item title={"API Docs"} link="#" onClick={close} />
+                <Nav.Item title={"Articles"} link="#" onClick={close} />
+                <Nav.Item title={"Request Data"} link="#" onClick={close} />
+              </>
             )}
-          </div>
+          </Nav>
         </div>
       </Container>
     </div>
-  );
-};
-
-const ThemeToggle = () => {
-  const { theme, setTheme } = useTheme();
-
-  return (
-    <>
-      <Button
-        className="group relative hover:bg-washed dark:hover:bg-washed-dark"
-        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      >
-        <Transition
-          show={theme === "light"}
-          enter="delay-200 transition ease-out duration-150"
-          enterFrom=" opacity-0 translate-y-1"
-          enterTo=" opacity-100 translate-y-0"
-          leave="duration-150"
-          leaveFrom="absolute opacity-100 translate-y-0"
-          leaveTo="absolute opacity-0 translate-y-1"
-        >
-          <MoonIcon className=" h-4 w-4 text-dim group-hover:text-black" />
-        </Transition>
-        <Transition
-          show={theme !== "light"}
-          enter="delay-200 transition ease-out duration-150"
-          enterFrom="opacity-0 translate-y-1"
-          enterTo="opacity-100 translate-y-0"
-          leave="duration-150"
-          leaveFrom="absolute  opacity-100 translate-y-0"
-          leaveTo="absolute  opacity-0 translate-y-1"
-        >
-          <SunIcon className="-m-0.5 h-5 w-5 text-dim dark:group-hover:text-white" />
-        </Transition>
-      </Button>
-    </>
   );
 };
 
--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -1,27 +1,107 @@
+import { Bars3BottomRightIcon, XMarkIcon } from "@heroicons/react/20/solid";
 import { clx } from "@lib/helpers";
-import { FunctionComponent, ReactNode } from "react";
+import At from "@components/At";
+import { useRouter } from "next/router";
+import { FunctionComponent, ReactNode, useState } from "react";
+import Dropdown from "@components/Dropdown";
+import { languages } from "@lib/options";
+import ThemeToggle from "./theme";
 
-type NavProps = {
-  isTabletNavOpen: boolean;
-  children: ReactNode;
+import { useLanguage } from "@hooks/useLanguage";
+
+type NavRootProps = {
+  children: (close: () => void) => ReactNode;
+  stateSelector?: ReactNode;
 };
 
-const Nav: FunctionComponent<NavProps> = ({ isTabletNavOpen, children }) => {
+type NavItemProps = {
+  icon?: ReactNode;
+  title: string;
+  link: string;
+  onClick: () => void;
+  className?: string;
+};
+
+type NavFunctionComponent = FunctionComponent<NavRootProps> & {
+  Item: typeof Item;
+};
+
+const Item: FunctionComponent<NavItemProps> = ({ link, onClick, className, icon, title }) => {
+  const { pathname } = useRouter();
   return (
-    <>
-      {/* MOBILE NAV MENU */}
-      <div
-        className={clx(
-          "fixed top-[57px] left-0 w-full flex-col gap-0 divide-y bg-white py-2 px-4 shadow-lg backdrop-blur-md dark:divide-washed-dark dark:bg-black/80 lg:hidden lg:gap-1 lg:divide-y-0 lg:p-1",
-          isTabletNavOpen ? "flex" : "hidden"
-        )}
-      >
-        {children}
-      </div>
-      {/* DESKTOP NAV BAR */}
-      <div className="hidden w-full gap-2 lg:flex">{children}</div>
-    </>
+    <At
+      href={link}
+      scroll={false}
+      onClick={onClick}
+      className={clx(
+        "flex items-center gap-2 rounded-none px-2 py-2 text-sm font-medium transition hover:cursor-pointer hover:bg-washed dark:hover:bg-washed-dark md:rounded-md md:py-[6px]",
+        pathname.startsWith(link) && link !== "/" ? "bg-washed dark:bg-washed-dark" : "",
+        className
+      )}
+    >
+      {icon}
+      {title}
+    </At>
   );
 };
+
+const Nav: NavFunctionComponent = ({ children, stateSelector }) => {
+  const [showMobile, setShowMobile] = useState<boolean>(false);
+  const { language, onLanguageChange } = useLanguage();
+
+  const close = () => setShowMobile(false);
+  const open = () => setShowMobile(true);
+
+  return (
+    <div className="w-full items-center justify-end lg:justify-between">
+      {/* Desktop */}
+      <div className="hidden w-fit gap-2 lg:flex">{children(close)}</div>
+      <div className="hidden w-fit gap-4 lg:flex">
+        {stateSelector}
+        <ThemeToggle />
+        <Dropdown
+          width="w-fit"
+          selected={languages.find(lang => lang.value === language)}
+          onChange={onLanguageChange}
+          options={languages}
+        />
+      </div>
+
+      {/* Mobile - Header*/}
+      <div className="flex w-full items-center justify-end gap-3 lg:hidden">
+        {stateSelector}
+        <ThemeToggle />
+        <Dropdown
+          width="w-fit"
+          selected={languages.find(lang => lang.value === language)}
+          onChange={onLanguageChange}
+          options={languages}
+        />
+        {showMobile ? (
+          <XMarkIcon
+            className="box-content block h-5 w-5 text-black dark:text-white lg:hidden"
+            onClick={close}
+          />
+        ) : (
+          <Bars3BottomRightIcon
+            className="box-content block h-5 w-5 text-black dark:text-white lg:hidden"
+            onClick={open}
+          />
+        )}
+      </div>
+      {/* Mobile - Menu */}
+      <div
+        className={clx(
+          "fixed top-[57px] left-0 flex w-full flex-col gap-0 divide-y bg-white py-2 px-4 shadow-lg backdrop-blur-md dark:divide-washed-dark dark:bg-black/80 lg:hidden lg:gap-1 lg:divide-y-0 lg:p-1",
+          showMobile ? "flex" : "hidden"
+        )}
+      >
+        {children(close)}
+      </div>
+    </div>
+  );
+};
+
+Nav.Item = Item;
 
 export default Nav;
