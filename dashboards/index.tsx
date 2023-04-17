78a03e76e8adf1415017b40377d8e9b9e1bdbc65
leneherng <49067812+leneherng@users.noreply.github.com>
2023-04-17 21:08:33 +0800

    fix: use h5 tag

--- dashboards/index.tsx
+++ dashboards/index.tsx
@@ -162,7 +162,7 @@ const DashboardIndex: FunctionComponent<DashboardIndexProps> = ({
                     className="my-3 inline-block h-min w-full rounded-xl border border-outline bg-background p-[18px] dark:border-washed-dark dark:bg-background-dark"
                     key={category}
                   >
-                    <div className="pb-1 text-lg font-bold">{category}</div>
+                    <h5 className="pb-1">{category}</h5>
                     {dashboards.map((item: Dashboard, index: number) => (
                       <div className="pt-2">
                         <At href={item.id} key={item.id}>
