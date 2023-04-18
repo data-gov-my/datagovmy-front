eb0e41c85fcd4eee9ae7a6fc837d8b84211f08d1
irfanismail <irfan.ismail96@gmail.com>
2023-04-19 06:26:39 +0800

    merge again?

---
+++ dashboards/democracy/election-explorer/index.tsx
@@ -0,0 +1,79 @@
+import { FunctionComponent } from "react";
+import AgencyBadge from "@components/AgencyBadge";
+import Hero from "@components/Hero";
+import Container from "@components/Container";
+import ContainerTabs from "@components/Tabs/ContainerTabs";
+import { SPRIcon, SPRIconSolid } from "@components/Icon/agency";
+import { useData } from "@hooks/useData";
+import { useTranslation } from "@hooks/useTranslation";
+import { FlagIcon, MapIcon, UserIcon } from "@heroicons/react/24/solid";
+import Election from "./elections";
+import ElectionCandidates from "./candidates";
+import ElectionParties from "./political-parties";
+import ElectionSeats from "./seats";
+
+/**
+ * Election Explorer Dashboard
+ * @overview Status: In-development
+ */
+
+interface ElectionExplorerProps {}
+
+const ElectionExplorer: FunctionComponent<ElectionExplorerProps> = ({}) => {
+  const { t, i18n } = useTranslation(["common", "dashboard-election-explorer"]);
+
+  const PANELS = [
+    {
+      name: t("dashboard-election-explorer:elections"),
+      icon: <SPRIconSolid className="-mb-1" />,
+      data: <Election />,
+    },
+    {
+      name: t("dashboard-election-explorer:candidates"),
+      icon: <UserIcon className="m-1 h-5 w-5" />,
+      data: <ElectionCandidates />,
+    },
+    {
+      name: t("dashboard-election-explorer:parties"),
+      icon: <FlagIcon className="m-1 h-5 w-5" />,
+      data: <ElectionParties />,
+    },
+    {
+      name: t("dashboard-election-explorer:seats"),
+      icon: <MapIcon className="m-1 h-5 w-5" />,
+      data: <ElectionSeats />,
+    },
+  ];
+  const { data, setData } = useData({
+    tabs: 0,
+  });
+  return (
+    <>
+      <Hero
+        background="red"
+        category={[t("nav.megamenu.categories.democracy"), "text-danger"]}
+        header={[t("dashboard-election-explorer:header")]}
+        description={[t("dashboard-election-explorer:description")]}
+        agencyBadge={
+          <AgencyBadge
+            agency={"Election Comission (EC)"}
+            link="https://www.spr.gov.my/"
+            icon={<SPRIcon />}
+          />
+        }
+      />
+
+      <Container className="min-h-fit">
+        <ContainerTabs current={data.tabs}>
+          {PANELS.map((panel, index) => (
+            <ContainerTabs.Panel name={panel.name} icon={panel.icon} key={index}>
+              {panel.data}
+            </ContainerTabs.Panel>
+          ))}
+        </ContainerTabs>
+      </Container>
+    </>
+  );
+};
+
+export default ElectionExplorer;
