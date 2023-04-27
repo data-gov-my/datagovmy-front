8a6627894d8f71a63bc569dc26c5382b076455f2
heheheejin <e_jaxx@hotmail.com>
2023-04-27 19:07:21 +0800

    wip: fetch and slice dropdown data

--- apps/app/pages/dashboard/sekolahku/[code].tsx
+++ apps/app/pages/dashboard/sekolahku/[code].tsx
@@ -31,23 +31,24 @@ const Sekolahku: Page = ({
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const dropdown_data = [
-    { code: "PEB1094", school: "PENANG FREE SCHOOL", postcode: "11600", state: "Pulau Pinang" },
-    { code: "JBA0072", school: "SK SRI BELAHAN", postcode: "83100", state: "Johor" },
-    { code: "JBA0071", school: "SK MUTIARA", postcode: "83100", state: "Johor" },
-    { code: "JBA0069", school: "SK RENGIT", postcode: "83100", state: "Johor" },
-    { code: "JEA0017", school: "SMK PERMATA JAYA", postcode: "83100", state: "Johor" },
-    { code: "JBC0013", school: "SJK(C) CHONG HWA RENGIT", postcode: "83100", state: "Johor" },
-    { code: "JEE0018", school: "SMK TUN SARDON", postcode: "83100", state: "Johor" },
-    { code: "JBB0022", school: "SK SRI RENGIT", postcode: "83100", state: "Johor" },
-    { code: "JBA0065", school: "SK SUNGAI TONGKANG", postcode: "83100", state: "Johor" },
-    { code: "JBA0066", school: "SK SERI SAMPURNA", postcode: "83100", state: "Johor" },
-    { code: "JBA0067", school: "SK SERI PERPAT", postcode: "83100", state: "Johor" },
-  ];
-
-  const paths = dropdown_data.map(school => {
-    return { params: { code: school.code } };
+  const { data: dropdown_data } = await get("/dropdown", { dashboard: "sekolahku" });
+  let paths: Array<any> = [];
+  dropdown_data.query_values.data.data.slice(0, 10).forEach((school: Record<string, string>) => {
+    paths = paths.concat([
+      {
+        params: {
+          code: school.code,
+        },
+      },
+      {
+        params: {
+          code: school.code,
+        },
+        locale: "ms-MY",
+      },
+    ]);
   });
+
   return {
     paths: paths,
     fallback: false, // can also be true or 'blocking'
@@ -59,23 +60,12 @@ export const getStaticProps: GetStaticProps = withi18n(
   async ({ params }) => {
     const { data } = await get("/dashboard", { dashboard: "sekolahku", code: params?.code });
 
-    const dropdown_data = [
-      { code: "PEB1094", school: "PENANG FREE SCHOOL", postcode: "11600", state: "Pulau Pinang" },
-      { code: "JBA0072", school: "SK SRI BELAHAN", postcode: "83100", state: "Johor" },
-      { code: "JBA0071", school: "SK MUTIARA", postcode: "83100", state: "Johor" },
-      { code: "JBA0069", school: "SK RENGIT", postcode: "83100", state: "Johor" },
-      { code: "JEA0017", school: "SMK PERMATA JAYA", postcode: "83100", state: "Johor" },
-      { code: "JBC0013", school: "SJK(C) CHONG HWA RENGIT", postcode: "83100", state: "Johor" },
-      { code: "JEE0018", school: "SMK TUN SARDON", postcode: "83100", state: "Johor" },
-      { code: "JBB0022", school: "SK SRI RENGIT", postcode: "83100", state: "Johor" },
-      { code: "JBA0065", school: "SK SUNGAI TONGKANG", postcode: "83100", state: "Johor" },
-      { code: "JBA0066", school: "SK SERI SAMPURNA", postcode: "83100", state: "Johor" },
-      { code: "JBA0067", school: "SK SERI PERPAT", postcode: "83100", state: "Johor" },
-    ];
+    const { data: dropdown_data } = await get("/dropdown", { dashboard: "sekolahku" });
+
     return {
       notFound: false,
       props: {
-        dropdown_data: dropdown_data,
+        dropdown_data: dropdown_data.query_values.data.data.slice(0, 10),
         sekolahku_info: data.sekolahku_info.data,
         bellcurve_school: data.bellcurve_school.data,
         bellcurve_callout: data.bellcurve_callout.data.data,
