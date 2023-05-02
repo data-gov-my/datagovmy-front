b53e82ea70cd1c79c8f33ee21904a6d8c80a97d3
irfanismail <irfan.ismail96@gmail.com>
2023-05-02 14:30:25 +0800

    fix: env & sitemap nextra

---
+++ apps/docs/next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.APP_URL || "https://developer.data.gov.my",
+  generateIndexSitemap: true,
+  generateRobotsTxt: true,
+  priority: 0.7,
+  autoLastmod: true,
+  outDir: "public",
+  exclude: ["/404", "/ms-MY/404", "500", "/ms-MY/500"],
+};
