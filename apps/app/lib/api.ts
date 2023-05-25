78994afc7e0b2bf1c8f02dc0287bc94372738834
Len E-Herng <leneherng@gmail.com>
2023-05-25 10:52:57 +0800

    refactor: allow custom headers

--- apps/app/lib/api.ts
+++ apps/app/lib/api.ts
@@ -3,13 +3,13 @@ import axios, { AxiosRequestConfig, AxiosRequestHeaders, AxiosResponse } from "a
 /**
  * Base URL builder for AKSARA.
  * @param base "api" | "local"
- * @param {boolean} contentTypeHeader add Content-Type: "multipart/form-data" to header
+ * @param {Record<string, string>} headers Additional headers
  * @returns Base of URL
  *
  * @example "api"   -> "https://[NEXT_PUBLIC_API_URL]/"
  * @example "local" -> "https://[NEXT_PUBLIC_APP_URL]/"
  */
-const instance = (base: "api" | "local" | string = "api", contentTypeHeader?: boolean) => {
+const instance = (base: "api" | "local" | string = "api", headers: Record<string, string> = {}) => {
   const config: AxiosRequestConfig = {
     baseURL:
       base === "api"
@@ -19,13 +19,9 @@ const instance = (base: "api" | "local" | string = "api", contentTypeHeader?: bo
         : base,
     headers: {
       Authorization: process.env.NEXT_PUBLIC_AUTHORIZATION_TOKEN,
+      ...headers,
     },
   };
-  if (contentTypeHeader)
-    config.headers = {
-      ...config.headers,
-      ["Content-Type"]: "multipart/form-data",
-    };
   return axios.create(config);
 };
 
@@ -54,17 +50,17 @@ export const get = (
  * @param route Endpoint route
  * @param payload Body payload
  * @param {"api" | "local"} base api | local
- * @param {boolean} contentTypeHeader add Content-Type: "multipart/form-data" to header
+ * @param {Record<string, string>} headers Additional headers
  * @returns {Promise<AxiosResponse>} Promised response
  */
 export const post = (
   route: string,
   payload?: any,
   base: "api" | "local" | string = "api",
-  contentTypeHeader: boolean = false
+  headers: Record<string, string> = {}
 ): Promise<AxiosResponse> => {
   return new Promise((resolve, reject) => {
-    instance(base, contentTypeHeader)
+    instance(base, headers)
       .post(route, payload)
       .then((response: AxiosResponse) => resolve(response))
       .catch(err => reject(err));
