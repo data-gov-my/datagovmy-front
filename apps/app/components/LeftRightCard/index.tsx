b31e483a1795f87f00d76952774930d78478ec97
Len E-Herng <leneherng@gmail.com>
2023-06-02 14:43:24 +0800

    style: prevent left from shrinking

--- apps/app/components/LeftRightCard/index.tsx
+++ apps/app/components/LeftRightCard/index.tsx
@@ -16,7 +16,7 @@ const LeftRightCard: FunctionComponent<CardProps> = ({ left, right }) => {
   return (
     <>
       <div className="flex flex-col items-stretch overflow-visible rounded-xl border border-slate-200 dark:border-zinc-800 lg:flex-row">
-        <div className="basis-1/3 overflow-visible border-slate-200 dark:border-zinc-800 dark:bg-zinc-800/50 lg:border-r">
+        <div className="shrink-0 basis-1/3 overflow-visible border-slate-200 dark:border-zinc-800 dark:bg-zinc-800/50 lg:border-r">
           {left}
         </div>
         <div className="basis-2/3 rounded-b-xl bg-slate-50 dark:bg-zinc-900 lg:rounded-r-xl">
