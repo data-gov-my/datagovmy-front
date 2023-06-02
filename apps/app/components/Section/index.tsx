240a1e87afaf21f28dc5f29680756258372d7f53
Len E-Herng <leneherng@gmail.com>
2023-06-02 23:23:32 +0800

    style: Section menu justify-start in mobile

--- apps/app/components/Section/index.tsx
+++ apps/app/components/Section/index.tsx
@@ -50,7 +50,7 @@ const Section: FunctionComponent<SectionProps> = forwardRef(
                 )}
               </div>
               {(description || menu) && (
-                <div className="text-dim flex flex-wrap gap-x-6 gap-y-3 md:flex-nowrap md:items-end md:justify-between">
+                <div className="text-dim flex flex-wrap gap-x-6 gap-y-3 md:flex-nowrap md:items-start md:justify-between">
                   {description && typeof description === "string" ? (
                     <p
                       className={[
@@ -63,7 +63,7 @@ const Section: FunctionComponent<SectionProps> = forwardRef(
                   ) : (
                     <div>{description}</div>
                   )}
-                  {menu && <div className="flex w-full justify-end gap-3 md:w-auto">{menu}</div>}
+                  {menu && <div className="flex w-full gap-3 md:w-auto md:justify-end">{menu}</div>}
                 </div>
               )}
             </div>
