9eaf305b0be7b402714026182eaa42bb76c78542
Len E-Herng <leneherng@gmail.com>
2023-06-12 18:17:46 +0800

    feat: initialise Civil Service dashboard

--- apps/app/pages/dashboard/civil-service/index.tsx
+++ apps/app/pages/dashboard/civil-service/index.tsx
@@ -7,19 +7,36 @@ import { useTranslation } from "@hooks/useTranslation";
 import CivilServiceDashboard from "@dashboards/public-finances/civil-service";
 import { withi18n } from "@lib/decorators";
 
-const CivilService: Page = ({}: InferGetStaticPropsType<typeof getStaticProps>) => {
+const CivilService: Page = ({
+  agencies,
+  barmeter,
+  choropleth,
+  last_updated,
+  pyramid,
+  timeseries,
+  timeseries_callout,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   const { t } = useTranslation(["common"]);
 
   return (
     <>
       <Metadata title={t("header")} description={t("description")} keywords={""} />
-      <CivilServiceDashboard />
+      <CivilServiceDashboard
+        agencies={agencies}
+        barmeter={barmeter}
+        choropleth={choropleth}
+        last_updated={last_updated}
+        pyramid={pyramid}
+        timeseries={timeseries}
+        timeseries_callout={timeseries_callout}
+      />
     </>
   );
 };
 // Disabled
 export const getStaticProps: GetStaticProps = withi18n("dashboard-civil-service", async () => {
-  //   const { data } = await get("/dashboard", { dashboard: "currency" });
+  const { data } = await get("/dashboard", { dashboard: "unhcr" });
+  const { data: timeseries } = await get("/dashboard", { dashboard: "immigration" });
 
   return {
     notFound: false,
@@ -30,6 +47,13 @@ export const getStaticProps: GetStaticProps = withi18n("dashboard-civil-service"
         category: "public-finances",
         agency: "JPA",
       },
+      agencies: [],
+      barmeter: data.barmeter,
+      choropleth: data.choropleth,
+      last_updated: Date.now(),
+      pyramid: [],
+      timeseries: timeseries.timeseries,
+      timeseries_callout: timeseries.timeseries_callout,
     },
   };
 });
