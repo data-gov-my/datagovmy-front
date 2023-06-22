84c7fcd735b189dd29ae9f95d2a5d958a35fa7f8
eujiaxin <e_jaxx@hotmail.com>
2023-06-22 16:35:15 +0800

    fix: remove blood donation outdated charts

--- apps/app/pages/dashboard/blood-donation/[state].tsx
+++ apps/app/pages/dashboard/blood-donation/[state].tsx
@@ -15,15 +15,9 @@ const BloodDonationState: Page = ({
   last_updated,
   params,
   timeseries_all,
-  timeseries_bloodstock,
-  timeseries_facility,
-  heatmap_bloodstock,
-  heatmap_donorrate,
-  heatmap_retention,
   barchart_age,
   barchart_time,
   barchart_variables,
-  map_facility,
   choropleth_malaysia_blood_donation,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const { t } = useTranslation(["dashboard-blood-donation", "common"]);
@@ -52,14 +46,8 @@ const BloodDonationState: Page = ({
         last_updated={last_updated}
         params={params}
         timeseries_all={timeseries_all}
-        timeseries_bloodstock={timeseries_bloodstock}
-        timeseries_facility={timeseries_facility}
-        heatmap_bloodstock={heatmap_bloodstock}
-        heatmap_donorrate={heatmap_donorrate}
-        heatmap_retention={heatmap_retention}
         barchart_age={barchart_age}
         barchart_time={barchart_time}
-        map_facility={map_facility}
         barchart_variables={{
           data_as_of: barchart_variables.data_as_of,
           data: vars,
@@ -93,22 +81,6 @@ BloodDonationState.layout = (page, props) => (
 
 // Build at runtime
 export const getStaticPaths: GetStaticPaths = async ctx => {
-  //   let paths: Array<any> = [];
-  //   STATES.filter(item => !["pjy", "pls", "lbn"].includes(item.key)).forEach(state => {
-  //     paths = paths.concat([
-  //       {
-  //         params: {
-  //           state: state.key,
-  //         },
-  //       },
-  //       {
-  //         params: {
-  //           state: state.key,
-  //         },
-  //         locale: "ms-MY",
-  //       },
-  //     ]);
-  //   });
   return {
     paths: [],
     fallback: "blocking",
@@ -121,10 +93,6 @@ export const getStaticProps: GetStaticProps = withi18n(
     const { data } = await get("/dashboard", { dashboard: "blood_donation", state: params?.state });
 
     // transfrom:
-    Object.values(data.heatmap_retention.data).forEach((item: any) => {
-      item.data = item.data.filter((_item: any) => _item.y !== null);
-    });
-
     data.bar_chart_time.data.monthly.x = data.bar_chart_time.data.monthly.x.map((item: any) => {
       const period = DateTime.fromFormat(item, "yyyy-MM-dd");
       return period.monthShort !== "Jan" ? period.monthShort : period.year.toString();
@@ -142,15 +110,9 @@ export const getStaticProps: GetStaticProps = withi18n(
         last_updated: new Date().valueOf(),
         params: params,
         timeseries_all: data.timeseries_all,
-        timeseries_bloodstock: data.timeseries_bloodstock,
-        timeseries_facility: data.timeseries_facility,
-        heatmap_donorrate: data.heatmap_donorrate,
-        heatmap_bloodstock: Object.values(data.heatmap_bloodstock),
-        heatmap_retention: Object.values(data.heatmap_retention),
         barchart_age: data.bar_chart_age,
         barchart_time: data.bar_chart_time,
         barchart_variables: data.barchart_key_variables,
-        map_facility: data.map_facility,
         choropleth_malaysia_blood_donation: data.choropleth_malaysia,
       },
     };
