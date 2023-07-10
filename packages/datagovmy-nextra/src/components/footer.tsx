1c050fd26b494b901696c4cc485ec90609c6e8ac
eujiaxin <e_jaxx@hotmail.com>
2023-07-11 00:27:49 +0800

    feat: update footer (harmonise)

--- packages/datagovmy-nextra/src/components/footer.tsx
+++ packages/datagovmy-nextra/src/components/footer.tsx
@@ -1,30 +1,69 @@
-import cn from "clsx";
 import type { ReactElement } from "react";
 import { useConfig } from "../contexts";
-import { renderComponent } from "../utils";
-import { LocaleSwitch } from "./locale-switch";
+import Image from "next/image";
 
 export function Footer({ menu }: { menu?: boolean }): ReactElement {
   const config = useConfig();
+  // TODO: i18n
   return (
-    <footer className="bg-gray-100 pb-[env(safe-area-inset-bottom)] dark:bg-neutral-900 print:bg-transparent">
-      <div
-        className={cn(
-          "mx-auto flex max-w-[90rem] gap-2 px-4 py-2",
-          menu && (config.i18n.length > 0 || config.darkMode) ? "flex" : "hidden"
-        )}
-      >
-        {config.i18n.length > 0 && <LocaleSwitch options={config.i18n} />}
-        {config.darkMode && renderComponent(config.themeSwitch.component)}
-      </div>
-      <hr className="dark:border-neutral-800" />
-      <div
-        className={cn(
-          "mx-auto flex max-w-[90rem] justify-center py-12 text-gray-600 dark:text-gray-400 md:justify-start",
-          "pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]"
-        )}
-      >
-        {renderComponent(config.footer.text)}
+    <footer className="bg-washed border-outline dark:border-washed-dark z-10 flex h-full w-full justify-center border-t pb-16 pt-12 dark:bg-black">
+      <div className="md:px-4.5 dark:divide-washed-dark undefined h-full w-full max-w-screen-2xl divide-y px-3 lg:px-6">
+        <div className="flex w-full flex-col gap-6 text-sm md:flex-row md:justify-between md:gap-0">
+          <div className="flex flex-row gap-4">
+            <div className="mt-1 w-12">
+              <Image src="/assets/jata_logo.png" width={48} height={36} alt="jata negara" />
+            </div>
+            <div>
+              <div className="mb-2 uppercase">
+                <p className="text-base font-bold">Government of Malaysia</p>
+              </div>
+              <p className="text-dim">© 2023 Public Sector Open Data</p>
+            </div>
+          </div>
+          <div className="flex flex-row gap-8 md:gap-14">
+            <div className="flex w-full flex-col gap-2 md:w-auto">
+              <p className="font-bold">Open Source</p>
+              <a
+                className="text-dim cursor-pointer text-sm hover:text-black hover:underline dark:hover:text-white"
+                href="https://github.com/data-gov-my/datagovmy-front"
+                target="_blank"
+              >
+                Frontend Repo: NextJS
+              </a>
+              <a
+                className="text-dim cursor-pointer text-sm hover:text-black hover:underline dark:hover:text-white"
+                href="https://github.com/data-gov-my/datagovmy-back"
+                target="_blank"
+              >
+                Backend Repo: Django
+              </a>
+              <a
+                className="text-dim cursor-pointer text-sm hover:text-black hover:underline dark:hover:text-white"
+                href="#"
+                //  TODO: add figma link (and open data links below)
+                target="_blank"
+              >
+                UI + UX Design: Figma
+              </a>
+            </div>
+            <div className="flex w-full flex-col gap-2 md:w-auto">
+              <p className="font-bold">Open Data</p>
+              <a
+                className="text-dim cursor-pointer text-sm hover:text-black hover:underline dark:hover:text-white"
+                href="#"
+                target="_blank"
+              >
+                Guiding Principles
+              </a>
+              <a
+                className="text-dim cursor-pointer text-sm hover:text-black hover:underline dark:hover:text-white"
+                href="/dashboard/car-popularity#"
+              >
+                Terms of Use
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </footer>
   );
