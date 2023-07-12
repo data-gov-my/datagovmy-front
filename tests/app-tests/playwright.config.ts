b113262977ce036f6e5274ae4134cea42489a59a
irfanismail <irfan.ismail96@gmail.com>
2023-07-12 20:16:45 +0800

    wip: testing ci github action

--- tests/app-tests/playwright.config.ts
+++ tests/app-tests/playwright.config.ts
@@ -6,6 +6,9 @@ import { defineConfig, devices } from "@playwright/test";
  */
 import dotenv from "dotenv";
 dotenv.config();
+
+const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
+const CI_MODE = process.env.CI === "true";
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -14,20 +17,17 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  // forbidOnly: !!process.env.CI,
+  // forbidOnly: !!CI_MODE,
   /* Retry on CI only */
-  // retries: process.env.CI ? 2 : 0,
-  retries: 1,
+  retries: CI_MODE ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  // workers: process.env.CI ? 1 : undefined,
-  workers: "50%",
+  workers: CI_MODE ? 1 : "50%",
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
+    baseURL: BASE_URL,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
@@ -79,6 +79,6 @@ export default defineConfig({
   // webServer: {
   //   command: 'npm run start',
   //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
+  //   reuseExistingServer: !CI_MODE,
   // },
 });
