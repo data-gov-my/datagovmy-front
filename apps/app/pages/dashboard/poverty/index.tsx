07cc6a414a760de1759843a2c476a9220679381a
Len E-Herng <leneherng@gmail.com>
2023-07-17 19:49:54 +0800

    feat: initialise Poverty dashboard

--- apps/app/pages/dashboard/poverty/index.tsx
+++ apps/app/pages/dashboard/poverty/index.tsx
@@ -8,29 +8,47 @@ import PovertyDashboard from "@dashboards/economy/poverty";
 import { withi18n } from "@lib/decorators";
 import { AnalyticsProvider } from "@hooks/useAnalytics";
 
-const Poverty: Page = ({ meta }: InferGetStaticPropsType<typeof getStaticProps>) => {
+const Poverty: Page = ({
+  choropleth,
+  heatmap,
+  last_updated,
+  meta,
+  timeseries,
+  timeseries_callout,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   const { t } = useTranslation(["dashboard-poverty", "common"]);
 
   return (
     <AnalyticsProvider meta={meta}>
       <Metadata title={t("header")} description={t("description")} keywords={""} />
-      <PovertyDashboard />
+      <PovertyDashboard
+        choropleth={choropleth}
+        heatmap={heatmap}
+        last_updated={last_updated}
+        timeseries={timeseries}
+        timeseries_callout={timeseries_callout}
+      />
     </AnalyticsProvider>
   );
 };
-// Disabled
+
 export const getStaticProps: GetStaticProps = withi18n("dashboard-poverty", async () => {
-  //   const { data } = await get("/dashboard", { dashboard: "currency" });
+  const { data } = await get("/dashboard", { dashboard: "poverty" });
 
   return {
     notFound: false,
     props: {
+      last_updated: data.data_last_updated,
       meta: {
         id: "dashboard-poverty",
         type: "dashboard",
         category: "economy",
         agency: "ICU JPM",
       },
+      choropleth: { state: data.choropleth_state, district: data.choropleth_district },
+      heatmap: data.heatmap,
+      timeseries: data.timeseries,
+      timeseries_callout: data.timeseries_callout,
     },
   };
 });
