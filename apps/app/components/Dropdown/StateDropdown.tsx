85306de246114afedaa2088471a6da971a24cbbd
irfanismail <irfan.ismail96@gmail.com>
2023-07-20 17:59:16 +0800

    chore: remove darkMode on dropdown

--- apps/app/components/Dropdown/StateDropdown.tsx
+++ apps/app/components/Dropdown/StateDropdown.tsx
@@ -34,7 +34,6 @@ const StateDropdown: FunctionComponent<StateDropdownProps> = ({
   sublabel,
   disabled = false,
   hideOnScroll = false,
-  darkMode = false,
 }) => {
   const { t } = useTranslation();
   const router = useRouter();
@@ -61,7 +60,6 @@ const StateDropdown: FunctionComponent<StateDropdownProps> = ({
         placeholder={t("common:placeholder.state")}
         enableFlag
         anchor={anchor}
-        darkMode={darkMode}
         width={width}
         sublabel={sublabel}
       />
