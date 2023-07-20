4fb1606f0aacbed49f7d9a3487fc7d5514a2f6c4
irfanismail <irfan.ismail96@gmail.com>
2023-07-20 16:06:12 +0800

    fix: codeblock support light theme

--- apps/app/components/CodeBlock/index.tsx
+++ apps/app/components/CodeBlock/index.tsx
@@ -1,15 +1,14 @@
 import Dropdown from "@components/Dropdown";
 import { DocumentDuplicateIcon, GlobeAltIcon } from "@heroicons/react/24/outline";
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent, useEffect, useMemo, useState } from "react";
 import hljs from "highlight.js/lib/core";
 import python from "highlight.js/lib/languages/python";
-// import julia from "highlight.js/lib/languages/julia";
-// import r from "highlight.js/lib/languages/r";
-import "highlight.js/styles/shades-of-purple.css";
+import { GithubThemes } from "./theme";
 import { OptionType } from "@components/types";
 import { copyClipboard } from "@lib/helpers";
 import { useTranslation } from "@hooks/useTranslation";
 import { track } from "@lib/mixpanel";
+import { useTheme } from "next-themes";
 
 interface CodeBlockProps {
   children: string;
@@ -18,9 +17,8 @@ interface CodeBlockProps {
 
 const CodeBlock: FunctionComponent<CodeBlockProps> = ({ children, event }) => {
   const { t } = useTranslation();
+  const { theme = "light" } = useTheme();
   hljs.registerLanguage("python", python);
-  //   hljs.registerLanguage("julia", julia);
-  //   hljs.registerLanguage("r", r);
   const languageOptions: OptionType[] = [
     {
       label: "Python",
@@ -29,6 +27,23 @@ const CodeBlock: FunctionComponent<CodeBlockProps> = ({ children, event }) => {
   ];
   const [language, setLanguage] = useState<OptionType>(languageOptions[0]);
   const [copyText, setCopyText] = useState<string>(t("common.copy"));
+
+  useEffect(() => {
+    const head = document.head;
+    let code_theme = document.getElementById("code-theme");
+    if (code_theme === null) {
+      code_theme = document.createElement("style");
+      code_theme.setAttribute("id", "code-theme");
+    }
+
+    code_theme.innerHTML = GithubThemes[theme as "light" | "dark"];
+    head.append(code_theme);
+  }, [theme]);
+
+  const html = useMemo<string>(
+    () => hljs.highlight(children, { language: language.value }).value,
+    [language, children]
+  );
 
   const handleCopy = () => {
     track("code_copy", { language: language.value, ...event });
@@ -39,10 +54,9 @@ const CodeBlock: FunctionComponent<CodeBlockProps> = ({ children, event }) => {
     }, 1000);
   };
   return (
-    <div className="dark:border-outlineHover-dark dark:bg-background-dark h-fit rounded-xl border bg-black">
-      <div className="border-outline flex justify-between border-b border-opacity-20 p-2.5 text-white">
+    <div className="dark:border-outlineHover-dark bg-background dark:bg-background-dark h-fit rounded-xl border">
+      <div className="border-outline flex justify-between border-b border-opacity-20 p-1">
         <Dropdown
-          darkMode
           className="flex-row items-center"
           sublabel={<GlobeAltIcon className="mr-2 h-4 w-4" />}
           options={languageOptions}
@@ -57,9 +71,9 @@ const CodeBlock: FunctionComponent<CodeBlockProps> = ({ children, event }) => {
       </div>
       <div className="p-4.5 text-xs">
         <code
-          className="whitespace-pre-wrap break-all text-white"
+          className="whitespace-pre-wrap break-all"
           dangerouslySetInnerHTML={{
-            __html: hljs.highlight(children, { language: language.value }).value,
+            __html: html,
           }}
         />
       </div>
