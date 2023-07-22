d0196698e200d7308d3317527fb20d50aa184943
嘉欣 <e_jaxx@hotmail.com>
2023-07-22 10:52:23 +0800

    Docs/working content (#169)
    * feat: add introduction and quickstart content
    
    * update: overview section content
    
    * wip: data catalogue api content
    
    * docs: update data catalogue api
    
    * docs: update data catalogue content
    
    * docs: update overview content
    
    * docs: update query syntax examples
    
    * docs: update overview and authentication section
    
    * docs: enclose <> tag for placeholders
    
    * docs: create gtfs static docs
    
    * docs: update data catalogue content
    
    * docs: add `limit` query param docs
    
    * docs: update content and mark todos
    
    * docs: update overview malay ver & redirects
    
    * Slight improvements
    - Renamed throttling to rate limits
    - Restructured rate limits table to clearly distinguish token usage
    - Renamed some sub-headings to be more explanatory (for better AI)
    - Switched required column 'No' to 'Optional'
    - General improvements to some writeups where I see opportunity
    
    * Forgot to stage these files
    
    * further descriptive improvements
    
    * fix: update docsRepositoryBase
    
    * feat: update navbar links
    
    * feat: update navbar links (ms)
    
    * update: remove github from navbar
    
    * feat: update footer (harmonise)
    
    * dist: update dist
    
    * feat: add helpdesk page
    
    * update: sidebar nesting for transport api
    
    * feat: create ai-helper page
    
    * update: remove example requests
    
    * update: add changelog and minor revision
    
    * feat: attempt to add authorisation
    
    * fix: matcher to not include images
    
    * feat: use basic auth instead
    
    * i18n: some changes
    
    * feat: add FAQ
    
    * fix: footer i18n
    
    * chore: add Button
    
    * undo: add Button to index
    
    * gitignore: dist/
    
    * test: dist
    
    * fix: dist
    
    * fix: href
    
    * style: add padding
    
    * fix: style.css export
    
    * fix: style.css
    
    * update: move FAQ from nav to sidebar
    
    * style: updating headings
    
    * update: extract overview section
    
    * update: extract static api section
    
    * update: extract realtime api section
    
    * fix: change page title from nextra -> openAPI
    
    * style:  justify-center footer component
    
    * fix: gtfs-r page key in _meta
    
    ---------
    
    Co-authored-by: Sheng Han Lim <shenghan@gmail.com>
    Co-authored-by: Len E-Herng <leneherng@gmail.com>
    Co-authored-by: irfanismail <irfan.ismail96@gmail.com>

--- apps/docs/middleware.ts
+++ apps/docs/middleware.ts
@@ -1,1 +1,23 @@
-export { locales as middleware } from "nextra/locales";
+import { NextRequest, NextResponse } from "next/server";
+import { locales } from "nextra/locales";
+
+// Triggers on relevant pages. Middleware to be removed on launch
+export const config = {
+  matcher: ["/", "/((?!_next/static|_next/image|favicon.ico|.png).*)"],
+};
+
+export const middleware = (request: NextRequest) => {
+  if (process.env.APP_ENV === "development") return locales(request);
+
+  const basicAuth = request.headers.get("authorization");
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1];
+    const [user, password] = atob(authValue).split(":");
+
+    if (user === "admin" && password === process.env.AUTHORIZATION_TOKEN) return locales(request);
+  } else {
+    request.nextUrl.pathname = "/api/authorize";
+    return NextResponse.rewrite(new URL("/api/authorize", request.url));
+  }
+  return locales(request);
+};
