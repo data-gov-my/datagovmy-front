2b42865dd242ee8abd7307dcf7dee0ecdf5971a3
Len E-Herng <leneherng@gmail.com>
2023-07-25 17:24:11 +0800

    fix: make text unselectable

--- apps/app/components/Tabs/index.tsx
+++ apps/app/components/Tabs/index.tsx
@@ -42,7 +42,7 @@ const List: FunctionComponent<ListProps> = ({ options, current, onChange, icons,
         <li
           key={option}
           className={clx(
-            "flex cursor-pointer self-center whitespace-nowrap rounded-full px-[10px] py-1 text-sm outline-none transition-colors",
+            "flex cursor-pointer select-none self-center whitespace-nowrap rounded-full px-2.5 py-1 text-sm outline-none transition-colors",
             current === index
               ? "bg-outline dark:bg-washed-dark font-medium text-black dark:text-white"
               : "text-dim bg-transparent hover:text-black dark:hover:text-white"
