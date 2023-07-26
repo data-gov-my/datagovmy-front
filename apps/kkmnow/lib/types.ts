72bab09da5af98f560a43f71ab0b23c66a3258c3
irfanismail <irfan.ismail96@gmail.com>
2023-07-26 11:30:45 +0800

    chore: cleanup lib

--- apps/kkmnow/lib/types.ts
+++ apps/kkmnow/lib/types.ts
@@ -1,9 +1,3 @@
-import type { ChartOptions } from "chart.js";
-import {
-  AnnotationOptions,
-  AnnotationPluginOptions,
-  LabelAnnotationOptions,
-} from "chartjs-plugin-annotation";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 import type { ReactNode } from "react";
@@ -16,38 +10,7 @@ export type Page = NextPage & {
   layout?: (page: ReactNode, props: Record<string, any>) => ReactNode;
 };
 
-// CHART INTERFACE
-
 export type TimeseriesOption = {
   period: "auto" | "month" | "year";
   periodly: "daily_7d" | "daily" | "monthly" | "yearly";
 };
-export interface IChart {
-  id: string;
-  keys: string[];
-  data: any;
-  [key: string]: any;
-}
-
-export type ChartCrosshairOption = ChartOptions & {
-  plugins: {
-    crosshair?:
-      | {
-          line: {
-            width?: number;
-            color?: string;
-            dashPattern?: [number, number];
-          };
-          zoom: {
-            enabled: boolean;
-          };
-          sync: {
-            enabled: boolean;
-          };
-        }
-      | false;
-    annotation?: AnnotationPluginOptions | false;
-  };
-};
-export type BarCrosshairOption = ChartOptions<"bar"> & ChartCrosshairOption;
-export type LineCrosshairOption = ChartOptions<"line"> & ChartCrosshairOption;
