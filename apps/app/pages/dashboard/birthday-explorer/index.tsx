a9f01ebf7364fced11128fdb5e0ab0e389e9e1c4
Len E-Herng <leneherng@gmail.com>
2023-07-26 09:30:27 +0800

    fix: notFound in prod

--- apps/app/pages/dashboard/birthday-explorer/index.tsx
+++ apps/app/pages/dashboard/birthday-explorer/index.tsx
@@ -24,6 +24,7 @@ export const getStaticProps: GetStaticProps = withi18n(
   async () => {
     const { data } = await get("/explorer", { explorer: "BIRTHDAY_POPULARITY", state: "mys" });
     return {
+      notFound: process.env.NEXT_PUBLIC_APP_ENV === "production",
       props: {
         meta: {
           id: "dashboard-birthday-explorer",
