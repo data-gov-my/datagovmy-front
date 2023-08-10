b0cb1cde477dac34698eba88edd4848ee433aea2
Irfan Ismail <irfan.ismail96@gmail.com>
2023-08-10 13:58:11 +0800

    feat: og docs (#201)

--- apps/docs/index.d.ts
+++ apps/docs/index.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     APP_URL: string;
     APP_ENV: string;
+    NEXT_PUBLIC_APP_URL: string;
     NEXT_PUBLIC_AI_URL: string;
     NEXT_PUBLIC_AI_TOKEN: string;
     NEXT_PUBLIC_APP_ENV: string;
---
+++ apps/docs/theme.config.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+import type { DocsThemeConfig } from "datagovmy-nextra";
+
+export default {
+  logo: (
+    <>
+      <Image src="/assets/logo.png" width={32} height={32} alt="data.gov.my" />
+      <h4 className="block">data.gov.my</h4>
+      <span
+        style={{
+          fontSize: "12px",
+          lineHeight: 1,
+          border: "2px solid",
+          borderRadius: 6,
+          padding: "2px 6px",
+          fontFamily: "Inter",
+          fontWeight: 700,
+        }}
+      >
+        API
+      </span>
+    </>
+  ),
+  useNextSeoProps: () => {
+    const { defaultLocale, locale } = useRouter();
+    const lang = locale || defaultLocale || "en";
+    const description: Record<string, string> = {
+      en: "Open API Documentation helps you to explore the extensive array of open datasets and realtime information produced by the Malaysian government via APIs. ",
+      ms: "Dokumentasi API Terbuka sedia membantu anda meneroka pelbagai set data terbuka dan maklumat masa nyata yang dihasilkan oleh kerajaan Malaysia melalui API",
+    };
+    return {
+      titleTemplate: `%s - Open API Docs`,
+      description: description[lang],
+      openGraph: {
+        description: description[lang],
+        images: [
+          {
+            url: `${process.env.NEXT_PUBLIC_APP_URL}/assets/og_${lang}.png`,
+            alt: `og_${lang}.png`,
+          },
+        ],
+      },
+    };
+  },
+  search: {
+    placeholder: () => {
+      const { defaultLocale, locale } = useRouter();
+      const lang = locale || defaultLocale || "en";
+      return lang === "en" ? "Search API docs" : "Cari dokumentasi API";
+    },
+  },
+  project: {
+    link: "https://github.com/data-gov-my/datagovmy-front",
+  },
+  i18n: [
+    { locale: "en", text: "English" },
+    { locale: "ms", text: "Bahasa Melayu" },
+  ],
+} satisfies DocsThemeConfig;
