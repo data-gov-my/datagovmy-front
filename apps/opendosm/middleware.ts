e4d92c4fd269d9a1c6386d006cff5c17d585b964
irfanismail <irfan.ismail96@gmail.com>
2023-08-18 02:25:01 +0800

    feat: configure middleware to set rolling token

---
+++ apps/opendosm/middleware.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+import { get } from "@vercel/edge-config";
+
+// Triggers on relevant pages. Authentication to be removed at launch
+export const config = {
+  matcher: ["/", "/dashboard/:path*", "/data-catalogue/:path*", "/publications/:path*"],
+};
+
+export async function middleware(request: NextRequest) {
+  const token = await get<string>("ROLLING_TOKEN");
+  const response = NextResponse.next();
+  response.cookies.set("nekot", token || "yikes", { path: "/", maxAge: 60 * 60 });
+  return response;
+}
