5995316f9296bfd568909b3e99dc39236f251a32
Len E-Herng <leneherng@gmail.com>
2023-08-17 23:31:31 +0800

    fix: add opacity to Progress

--- packages/datagovmy-ui/src/components/Progress/index.tsx
+++ packages/datagovmy-ui/src/components/Progress/index.tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 
 const Progress: FunctionComponent = () => {
   const [progress, setProgress] = useState<number>(0);
+  const [opacity, setOpacity] = useState<number>(1);
   const { events, route } = useRouter();
 
   useEffect(() => {
@@ -30,29 +31,37 @@ const Progress: FunctionComponent = () => {
     };
 
     const startLoading = (url: string) => {
-        if (route === url) return;
-        clearTimeout(timeout);
-        clearInterval(trickle);
+      if (route === url) return;
+      clearTimeout(timeout);
+      clearInterval(trickle);
 
-        // start only if page yet to load after 400ms
-        timeout = setTimeout(() => {
-          setProgress(0);
-        }, 400);
+      // start only if page yet to load after 400ms
+      timeout = setTimeout(() => {
+        setProgress(0);
+        setOpacity(1);
+      }, 450);
 
+      if (opacity) {
         gradualTimeout(progress => {
           setProgress(progress);
         });
-      },
-      endLoading = () => {
-        clearTimeout(timeout);
-        clearInterval(trickle);
+      }
+    };
+    const endLoading = (url: string) => {
+      if (route === url) return;
+      clearTimeout(timeout);
+      clearInterval(trickle);
+
+      if (opacity) {
         setTimeout(() => {
           setProgress(100);
         }, 100);
         setTimeout(() => {
           setProgress(0);
+          setOpacity(0);
         }, 300);
-      };
+      }
+    };
 
     events.on("routeChangeStart", startLoading);
     events.on("routeChangeComplete", endLoading);
@@ -67,11 +76,12 @@ const Progress: FunctionComponent = () => {
 
   return (
     <div className="fixed left-0 top-0 z-50 h-[3px] w-full bg-transparent">
-      {progress > 0 && (
+      {opacity === 1 && (
         <div
-          className="bg-primary dark:bg-primary-dark h-full transition-[width] duration-300 ease-out"
+          className="bg-primary dark:bg-primary-dark h-full transition-[width,_opacity] duration-300 ease-out"
           style={{
             width: `${progress}%`,
+            opacity: opacity,
           }}
         />
       )}
