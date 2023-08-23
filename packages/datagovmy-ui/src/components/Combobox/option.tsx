8d1b2e62cc8087173c809cd4bcaf0168a412f773
Len E-Herng <leneherng@gmail.com>
2023-08-23 16:20:29 +0800

    feat: virtualise combobox

--- packages/datagovmy-ui/src/components/Combobox/option.tsx
+++ packages/datagovmy-ui/src/components/Combobox/option.tsx
@@ -1,41 +1,70 @@
 import { clx } from "../../lib/helpers";
 import ImageWithFallback from "../ImageWithFallback";
 import { OptionType } from "../../../types";
-import { Combobox } from "@headlessui/react";
 import { CheckCircleIcon } from "@heroicons/react/24/solid";
-import { ReactNode } from "react";
+import { CSSProperties, forwardRef, ReactNode } from "react";
+import { useId } from "@floating-ui/react";
 
 export type ComboOptionProp<T extends unknown> = OptionType & T;
 
 export type ComboOptionProps<T> = {
   option: ComboOptionProp<T>;
   format?: (option: ComboOptionProp<T>) => ReactNode;
+  onClick?: () => void;
   enableFlag?: boolean;
   imageSource?: string;
   fallback?: ReactNode;
+  style?: CSSProperties;
+  isSelected: boolean;
+  active: boolean;
+  index: number;
+  total: number;
 };
 
-const ComboOption = <T extends unknown>({
-  option,
-  format,
-  enableFlag,
-  imageSource,
-  fallback,
-}: ComboOptionProps<T>) => {
-  return (
-    <Combobox.Option
-      className={({ active }) =>
-        clx(
+const ComboOption = forwardRef<
+  HTMLDivElement,
+  ComboOptionProps<T> & React.HTMLProps<HTMLDivElement>
+>(
+  (
+    {
+      option,
+      format,
+      enableFlag,
+      imageSource,
+      fallback,
+      style,
+      onClick,
+      isSelected,
+      active,
+      index,
+      total,
+      ...rest
+    },
+    ref
+  ) => {
+    const id = useId();
+
+    return (
+      <div
+        id={id}
+        ref={ref}
+        style={style}
+        role="option"
+        aria-selected={active}
+        onClick={onClick}
+        {...rest}
+        // As the list is virtualized, this lets the assistive tech know
+        // how many options there are total without looking at the DOM.
+        aria-setsize={total}
+        aria-posinset={index + 1}
+        className={clx(
           "relative flex w-full cursor-pointer select-none flex-row gap-2 px-4 py-2",
           active && "bg-washed dark:bg-washed-dark"
-        )
-      }
-      value={option}
-    >
-      {({ selected }) => (
+        )}
+      >
         <>
           {format ? (
-            <p className={clx("flex gap-x-1 truncate", selected ? "font-medium" : "font-normal")}>
+            <p className={clx("flex gap-x-1 truncate", isSelected ? "font-medium" : "font-normal")}>
               {format(option)}
             </p>
           ) : (
@@ -46,34 +75,37 @@ const ComboOption = <T extends unknown>({
                     className="border-outline dark:border-outlineHover-dark rounded border"
                     src={`${imageSource}${option.value}.png`}
                     fallback={fallback}
-                    width={32}
+                    width={28}
                     height={18}
                     alt={option.value as string}
                     style={{
                       width: "auto",
-                      maxWidth: "32px",
+                      maxWidth: "28px",
                       height: "auto",
-                      maxHeight: "32px",
+                      maxHeight: "28px",
                     }}
                   />
                 </div>
               )}
               <p
-                className={clx("block grow self-center", selected ? "font-medium" : "font-normal")}
+                className={clx(
+                  "block grow self-center",
+                  isSelected ? "font-medium" : "font-normal"
+                )}
               >
                 {option.label}
               </p>
             </>
           )}
-          {selected && (
+          {isSelected && (
             <span className="absolute inset-y-0 right-3 flex items-center">
               <CheckCircleIcon className="text-primary dark:text-primary-dark h-4 w-4" />
             </span>
           )}
         </>
-      )}
-    </Combobox.Option>
-  );
-};
+      </div>
+    );
+  }
+);
 
 export default ComboOption;
