fdf75afe6ba5986b9d3bd66c74f3dae72dae6ab1
Len E-Herng <leneherng@gmail.com>
2023-08-25 15:44:59 +0800

    feat: vehicle type timeseries

--- apps/app/dashboards/transportation/car-popularity/index.tsx
+++ apps/app/dashboards/transportation/car-popularity/index.tsx
@@ -1,78 +1,106 @@
-import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 import { get } from "datagovmy-ui/api";
 import {
   AgencyBadge,
-  Button,
-  Card,
   Container,
   Dropdown,
   Hero,
+  List,
   Section,
+  Slider,
   toast,
 } from "datagovmy-ui/components";
-import { AKSARA_COLOR, BREAKPOINTS } from "datagovmy-ui/constants";
-import { WindowContext } from "datagovmy-ui/contexts/window";
-import { useData, useTranslation } from "datagovmy-ui/hooks";
-import { OptionType } from "datagovmy-ui/types";
+import { AKSARA_COLOR } from "datagovmy-ui/constants";
+import { SliderProvider } from "datagovmy-ui/contexts/slider";
+import { numFormat, toDate } from "datagovmy-ui/helpers";
+import { useData, useSlice, useTranslation } from "datagovmy-ui/hooks";
+import { OptionType, TimeseriesOption, WithData } from "datagovmy-ui/types";
 import dynamic from "next/dynamic";
-import { FunctionComponent, useContext, useMemo } from "react";
+import { FunctionComponent, useMemo } from "react";
 
 /**
  * Car Popularity Dashboard
- * @overview Status: In-development
+ * @overview Status: Live
  */
 
 interface CarPopularityProps {
   last_updated: string;
+  model: WithData<Record<string, number[]>>;
   queryOptions: Record<string, any>;
   tableData: Record<string, any>;
+  timeseries: WithData<Record<string, Record<string, number[]>>>;
+  timeseries_callout: WithData<Record<string, Record<string, number>>>;
 }
 
 const Timeseries = dynamic(() => import("datagovmy-ui/charts/timeseries"), { ssr: false });
 
 const CarPopularity: FunctionComponent<CarPopularityProps> = ({
   last_updated,
+  model,
   queryOptions,
   tableData,
+  timeseries,
+  timeseries_callout,
 }) => {
-  const { t } = useTranslation(["dashboard-car-popularity", "common"]);
-  const { size } = useContext(WindowContext);
+  const { t, i18n } = useTranslation(["dashboard-car-popularity", "common"]);
+  const config: { [key: string]: TimeseriesOption } = {
+    0: {
+      period: "auto",
+      periodly: "daily_7d",
+    },
+    1: {
+      period: "auto",
+      periodly: "daily",
+    },
+    2: {
+      period: "month",
+      periodly: "monthly",
+    },
+    3: {
+      period: "year",
+      periodly: "yearly",
+    },
+  };
 
   // query data
   const { data: query, setData: setQuery } = useData({
-    maker: { label: Object.keys(queryOptions)[0], value: Object.keys(queryOptions)[0] },
-    model: null,
+    maker: "",
+    model: "",
     params: {},
-    loading: false,
   });
 
   const yearOptions: OptionType[] = Object.keys(tableData.top_makers.data).map(val => {
     return { label: new Date(val).getFullYear().toString(), value: val };
   });
 
-  // table & timeseries data
   const { data, setData } = useData({
-    x: [],
-    y: [],
-    placeholderX: [
-      631152000000, 662688000000, 694224000000, 725846400000, 757382400000, 788918400000,
-      820454400000, 852076800000, 883612800000, 915148800000, 946684800000, 978307200000,
-      1009843200000, 1041379200000, 1072915200000, 1104537600000, 1136073600000, 1167609600000,
-      1199145600000, 1230768000000, 1262304000000, 1293840000000, 1325376000000, 1356998400000,
-      1388534400000, 1420070400000, 1451606400000, 1483228800000, 1514764800000, 1546300800000,
-      1577836800000, 1609459200000, 1640995200000,
-    ],
-    placeholderY: [
-      70150, 29766, 48832, 87217, 22895, 16223, 76955, 73224, 94678, 47467, 6086, 16614, 75976,
-      78252, 60954, 88490, 15135, 17795, 43085, 54941, 20484, 35517, 91158, 68620, 59882, 94217,
-      3477, 45860, 82643, 6464, 554, 16730, 20919,
-    ],
+    // timeseries data
+    index: 0,
+    loading: false,
+    minmax: [0, timeseries.data.daily.x.length - 1],
+    period: "auto",
+    periodly: "daily_7d",
+
+    data_as_of: model.data_as_of,
+    x: model.data.x,
+    y: model.data.cars,
 
     // table data
     selectedYear: yearOptions.at(-1),
     topMakers: tableData.top_makers.data[yearOptions.at(-1)?.value || -1],
     topModels: tableData.top_models.data[yearOptions.at(-1)?.value || -1],
   });
+
+  const { coordinate } = useSlice(timeseries.data[data.periodly], data.minmax);
+  const LATEST_TIMESTAMP =
+    timeseries.data[data.periodly].x[timeseries.data[data.periodly].x.length - 1];
+
+  const past_5yrs = useMemo<number>(() => {
+    return data.y.slice(data.y.length - 5, data.y.length).reduce((a: number, b: number) => a + b);
+  }, [data.y]);
+
+  const since_2000 = useMemo<number>(() => {
+    return data.y.reduce((a: number, b: number) => a + b);
+  }, [data.y]);
 
   const filterMakers = useMemo<Array<OptionType>>(() => {
     const _makers = Object.keys(queryOptions).map(maker => {
@@ -87,30 +115,24 @@ const CarPopularity: FunctionComponent<CarPopularityProps> = ({
       _models = queryOptions[query.maker.value].map((model: string) => {
         return { label: model, value: model };
       });
-      setQuery("model", _models[0]);
     }
     return _models;
   }, [query.maker]);
 
-  const searchHandler = () => {
-    setQuery("loading", true);
-
-    const params = {
-      maker: query.maker.value,
-      model: query.model.value,
-    };
-
+  const searchModel = (maker: string, model: string) => {
+    setData("loading", true);
     get("chart/", {
       dashboard: "car_popularity",
       chart_name: "timeseries",
-      ...params,
+      maker,
+      model,
     })
       .then(({ data }) => {
         setData("x", data.data?.x);
         setData("y", data.data?.cars);
         setData("data_as_of", data.data_as_of);
-        setData("params", params); // for timeseries title
-        setQuery("loading", false);
+        setQuery("params", { maker, model }); // for timeseries title
+        setData("loading", false);
       })
       .catch(e => {
         toast.error(t("common:error.toast.request_failure"), t("common:error.toast.try_again"));
@@ -131,7 +153,7 @@ const CarPopularity: FunctionComponent<CarPopularityProps> = ({
         {/* Best selling cars models and brands in {year} */}
         <Section>
           <div className="space-y-6">
-            <div className="flex flex-col place-content-center place-items-center gap-3 sm:flex-row">
+            <div className="flex w-full flex-col place-content-center place-items-center gap-3 sm:flex-row">
               <h4 className="text-center">{t("table_title")}</h4>
               <Dropdown
                 width="w-fit"
@@ -228,119 +250,158 @@ const CarPopularity: FunctionComponent<CarPopularityProps> = ({
         </Section>
 
         {/* How popular is your car? */}
-        <Section title={t("section_title")} date={data.data_as_of}>
-          <div className="flex flex-col gap-8 lg:grid lg:grid-cols-3">
-            <div className="w-full lg:col-span-1">
-              <Card className="border-outline bg-background dark:border-washed-dark dark:bg-washed-dark/50 shadow-button flex w-full flex-col justify-items-start	gap-6 rounded-xl border p-6">
-                <Dropdown
-                  label={t("label_maker")}
-                  placeholder={t("option_maker")}
-                  width="w-full"
-                  options={filterMakers}
-                  selected={query.maker}
-                  onChange={selected => setQuery("maker", selected)}
-                  enableSearch
-                />
-                <Dropdown
-                  label={t("label_model")}
-                  placeholder={t("option_model")}
-                  width="w-full"
-                  options={filterModels}
-                  selected={query.model}
-                  onChange={selected => setQuery("model", selected)}
-                  enableSearch
-                />
-                <div>
-                  <Button
-                    icon={<MagnifyingGlassIcon className=" h-4 w-4" />}
-                    className="btn-primary"
-                    onClick={searchHandler}
-                  >
-                    {t("search_button")}
-                  </Button>
-                </div>
-              </Card>
+        <section className="py-8 lg:py-12">
+          <div className="flex flex-col gap-y-6">
+            <h4 className="text-center">{t("section_title")}</h4>
+            <div className="flex items-center justify-center gap-x-3">
+              <Dropdown
+                placeholder={t("manufacturer")}
+                width="w-full sm:w-[200px]"
+                options={filterMakers}
+                selected={query.maker}
+                onChange={selected => setQuery("maker", selected)}
+                enableSearch
+              />
+              <Dropdown
+                placeholder={t("model")}
+                disabled={!query.maker}
+                width="w-full sm:w-[200px]"
+                options={filterModels}
+                selected={query.model}
+                onChange={selected => {
+                  setQuery("model", selected);
+                  searchModel(query.maker.value, selected.value);
+                }}
+                enableSearch={filterModels.length > 10}
+              />
             </div>
-            <div className="lg:col-span-2">
+          </div>
+
+          {/* {Model}: Popularity over the Years */}
+          <Section
+            className="mx-auto pt-8 lg:w-2/3"
+            title={
+              <h5>
+                {t("popularity_over_the_years", {
+                  manufacturer: query.params.maker ?? "Perodua",
+                  model: query.params.model ?? "Myvi",
+                })}
+              </h5>
+            }
+            description={t("popularity_disclaimer")}
+            date={data.data_as_of}
+          >
+            <Timeseries
+              className="h-[300px]"
+              isLoading={data.loading}
+              precision={0}
+              suggestedMaxY={2}
+              interval="year"
+              data={{
+                labels: data.x,
+                datasets: [
+                  {
+                    type: "line",
+                    data: data.y,
+                    label: t("cars"),
+                    backgroundColor: AKSARA_COLOR.PRIMARY_H,
+                    borderColor: AKSARA_COLOR.PRIMARY,
+                    borderWidth: 1.5,
+                    fill: true,
+                  },
+                ],
+              }}
+              stats={[
+                {
+                  title: t("this_year"),
+                  value: numFormat(data.y[data.y.length - 1], "standard"),
+                },
+                {
+                  title: t("past_5yrs"),
+                  value: numFormat(past_5yrs, "standard"),
+                },
+                {
+                  title: t("since_2000"),
+                  value: numFormat(since_2000, "standard"),
+                },
+              ]}
+            />
+          </Section>
+        </section>
+
+        {/* How are vehicle registrations trending? */}
+        <Section
+          title={t("registration_trend")}
+          description={t("registration_disclaimer")}
+          date={timeseries.data_as_of}
+          menu={
+            <List
+              current={data.index}
+              onChange={index => {
+                setData("index", index);
+                setData("minmax", [0, timeseries.data[config[index].periodly].x.length - 1]);
+                setData("period", config[index].period);
+                setData("periodly", config[index].periodly);
+              }}
+              options={[
+                t("common:time.daily_7d"),
+                t("common:time.daily"),
+                t("common:time.monthly"),
+                t("common:time.yearly"),
+              ]}
+            />
+          }
+        >
+          <SliderProvider>
+            {play => (
               <>
-                {data.x?.length > 0 ? (
-                  <Timeseries
-                    className="h-[300px]"
-                    isLoading={query.loading}
-                    precision={0}
-                    suggestedMaxY={2}
-                    title={
-                      <div className="flex flex-col gap-3">
-                        <p className="text-lg font-bold">
-                          <span className="capitalize">
-                            {t("timeseries_car_description", {
-                              car: data.params.model,
-                              maker: data.params.maker,
-                            })}
-                          </span>
-                          <span>{t("timeseries_title")}</span>
-                        </p>
-                        <p className="text-dim text-sm">
-                          <span>{t("timeseries_description")}</span>
-                        </p>
-                      </div>
-                    }
-                    interval={"year"}
-                    data={{
-                      labels: data.x,
-                      datasets: [
-                        {
-                          type: "line",
-                          data: data.y,
-                          label: t("label"),
-                          backgroundColor: AKSARA_COLOR.PRIMARY_H,
-                          borderColor: AKSARA_COLOR.PRIMARY,
-                          borderWidth:
-                            size.width <= BREAKPOINTS.MD
-                              ? 0.75
-                              : size.width <= BREAKPOINTS.LG
-                              ? 1.0
-                              : 1.5,
-                          fill: true,
-                        },
-                      ],
-                    }}
-                  />
-                ) : (
-                  <div className="relative hidden h-96 items-center justify-center lg:flex">
+                <div className="grid grid-cols-1 gap-12 md:grid-cols-2 xl:grid-cols-3">
+                  {["car", "motorcycle", "lorry", "van", "bus", "other"].map((key: string) => (
                     <Timeseries
-                      className="absolute left-0 top-0 h-full w-full opacity-30"
+                      key={key}
+                      title={t(key)}
+                      className="h-[300px] w-full"
+                      enableAnimation={!play}
+                      interval={data.period}
                       data={{
-                        labels: data.placeholderX,
+                        labels: coordinate.x,
                         datasets: [
                           {
                             type: "line",
-                            data: data.placeholderY,
+                            data: coordinate[key],
+                            label: t(`common:time.${data.periodly}`),
+                            fill: true,
                             backgroundColor: AKSARA_COLOR.PRIMARY_H,
                             borderColor: AKSARA_COLOR.PRIMARY,
-                            borderWidth:
-                              size.width <= BREAKPOINTS.MD
-                                ? 0.75
-                                : size.width <= BREAKPOINTS.LG
-                                ? 1.0
-                                : 1.5,
-                            fill: true,
+                            borderWidth: 1.5,
                           },
                         ],
                       }}
-                      enableGridX={false}
-                      enableCrosshair={false}
-                      interval={"year"}
+                      stats={[
+                        {
+                          title: t("this_year", {
+                            date: toDate(LATEST_TIMESTAMP, "MMM yyyy", i18n.language),
+                          }),
+                          value: `+${numFormat(timeseries_callout.data[key].latest, "standard")}`,
+                        },
+                        {
+                          title: t("alltime"),
+                          value: numFormat(timeseries_callout.data[key].alltime, "standard"),
+                        },
+                      ]}
                     />
-                    <Card className="border-outline bg-outline dark:border-washed-dark dark:bg-washed-dark z-10 flex h-min w-fit flex-row items-center gap-2 rounded-md border px-3 py-1.5 md:mx-auto">
-                      <MagnifyingGlassIcon className=" h-4 w-4" />
-                      <p>{t("search_prompt")}</p>
-                    </Card>
-                  </div>
-                )}
+                  ))}
+                </div>
+                <Slider
+                  type="range"
+                  period={data.period}
+                  value={data.minmax}
+                  data={timeseries.data[data.periodly].x}
+                  onChange={e => setData("minmax", e)}
+                />
               </>
-            </div>
-          </div>
+            )}
+          </SliderProvider>
         </Section>
       </Container>
     </>
