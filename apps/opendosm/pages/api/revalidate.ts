f4fdc115fbc799d3fa6657c9ff4b66536cc6cbc7
amalasyrafs <amaluddinasyraf@gmail.com>
2023-09-08 11:19:16 +0800

    refactor opendosm and kkmnow revalidation api

--- apps/opendosm/pages/api/revalidate.ts
+++ apps/opendosm/pages/api/revalidate.ts
@@ -1,3 +1,5 @@
+import { static_routes, routes as all_routes } from "@lib/routes";
+import { STATES } from "datagovmy-ui/constants";
 import { NextApiRequest, NextApiResponse } from "next";
 
 type RevalidateData = {
@@ -26,9 +28,17 @@ export default async function handler(
     const { route: _route }: { route: string } = req.body;
     if (!_route) throw new Error("Route(s) missing");
 
-    let routes: string[] = _route.split(",");
+    const routes: string[] = _route.split(",");
 
-    await Promise.all(routes.map(async route => rebuild(res, route, routes)));
+    await Promise.all(
+      routes.map(async route =>
+        validate(route)
+          .then(valid_route => rebuild(res, valid_route, routes))
+          .catch(e => {
+            throw new Error(e);
+          })
+      )
+    );
 
     return res.json({ message: "Revalidation successful", revalidated: routes });
   } catch (err: any) {
@@ -37,9 +47,45 @@ export default async function handler(
       .json({ error: "Revalidation failed", message: err.message, revalidated: [] });
   }
 }
+
+// Checks if route exists. Routes only valid for static pages
+const validate = (route: string): Promise<string> =>
+  new Promise((resolve, reject) => {
+    if (static_routes.includes(route)) resolve(route);
+    else reject(`Route does not exist or is not a static page. Route: ${route}`);
+  });
+
 // Rebuilds the relevant page(s).
 const rebuild = async (res: NextApiResponse, route: string, routes: string[]) =>
   new Promise(async (resolve, reject) => {
-    await res.revalidate(route).catch(e => reject(e));
-    resolve(true);
+    switch (route) {
+      case all_routes.POPULATION:
+      case all_routes.KAWASANKU:
+      case all_routes.HOUSEHOLD_INCOME_EXPENDITURE:
+      case `/ms-MY${all_routes.POPULATION}`:
+      case `/ms-MY${all_routes.KAWASANKU}`:
+      case `/ms-MY${all_routes.HOUSEHOLD_INCOME_EXPENDITURE}`:
+        await res.revalidate(route);
+        const result = revalidateWithStates(res, route);
+        routes.push.apply(routes, result);
+        resolve(true);
+        break;
+
+      // Simple route
+      default:
+        await res.revalidate(route).catch(e => reject(e));
+        resolve(true);
+        break;
+    }
   });
+
+const revalidateWithStates = (res: NextApiResponse, route: string, except?: string[]): string[] => {
+  let states = except ? STATES.filter(item => !except?.includes(item.key)) : STATES;
+  states.forEach(
+    async state =>
+      await res.revalidate(route.concat("/", state.key)).catch(e => {
+        throw new Error(e);
+      })
+  );
+  return states.map(({ key }) => route.concat("/", key));
+};
