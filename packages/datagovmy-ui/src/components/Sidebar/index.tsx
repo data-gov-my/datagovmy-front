085864438c99171b79c78368e05f1ba197ea4c68
Len E-Herng <leneherng@gmail.com>
2023-09-11 18:48:11 +0800

    add padding to sidebar

--- packages/datagovmy-ui/src/components/Sidebar/index.tsx
+++ packages/datagovmy-ui/src/components/Sidebar/index.tsx
@@ -25,8 +25,8 @@ const Sidebar: FunctionComponent<SidebarProps> = ({ children, categories, onSele
     <>
       <div className="flex w-full flex-row">
         {/* Desktop */}
-        <div className="dark:border-r-washed-dark hidden border-r lg:block lg:w-1/4 xl:w-1/5">
-          <ul className="sticky top-14 flex h-[calc(100vh-56px)] flex-col gap-2 overflow-auto py-3">
+        <div className="dark:border-r-washed-dark hidden border-r pb-6 lg:block lg:w-1/4 xl:w-1/5">
+          <ul className="sticky top-14 flex h-[calc(100vh-56px)] flex-col gap-2 overflow-auto pt-3">
             <li>
               <h5 className={styles.base}>{t("category")}</h5>
             </li>
