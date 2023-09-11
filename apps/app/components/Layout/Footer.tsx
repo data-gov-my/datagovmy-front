683c92ae6c09b2176c38261a756e227e368f4264
Len E-Herng <leneherng@gmail.com>
2023-09-11 14:19:44 +0800

    fix Header, Footer

--- apps/app/components/Layout/Footer.tsx
+++ apps/app/components/Layout/Footer.tsx
@@ -10,13 +10,13 @@ const DGMYFooter = () => {
       <div className="flex w-full flex-col gap-2 md:w-[200px]">
         <p className="font-bold">{t("common:nav.open_source")}</p>
 
-        <At className="link-dim" external href="#">
+        <At className="link-dim" external href="https://github.com/data-gov-my/datagovmy-front">
           {t("common:nav.frontend")}
         </At>
-        <At className="link-dim" external href="#">
+        <At className="link-dim" external href="https://github.com/data-gov-my/datagovmy-back">
           {t("common:nav.backend")}
         </At>
-        <At className="link-dim" external href="#">
+        <At className="link-dim" external href="https://www.figma.com/file/6iNojR8hO5bWvH0c3rvGD4">
           {t("common:nav.uiux")}
         </At>
       </div>
@@ -25,11 +25,15 @@ const DGMYFooter = () => {
       <div className="flex w-full flex-col gap-2 md:w-[200px]">
         <p className="font-bold">{t("common:nav.open_data")}</p>
 
-        <At className="link-dim" external href="#">
+        <At
+          className="link-dim"
+          external
+          href="https://dasar.mampu.gov.my/search-g/download-file/25/7f821c650c868d025fb5351d7d45d001"
+        >
           {t("common:nav.guiding_principles")}
         </At>
-        <At className="link-dim" external href="#">
-          {t("common:nav.terms_of_use")}
+        <At className="link-dim" external href="https://open.dosm.gov.my">
+          OpenDOSM
         </At>
       </div>
     </Footer>
