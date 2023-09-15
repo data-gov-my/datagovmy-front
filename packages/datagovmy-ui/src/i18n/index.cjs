ec85c5953b357485cc227f206644cccec2dfd60d
irfanismail <irfan.ismail96@gmail.com>
2023-09-15 09:55:34 +0800

    hotfix: i18n fs

--- packages/datagovmy-ui/src/i18n/index.cjs
+++ packages/datagovmy-ui/src/i18n/index.cjs
@@ -6,23 +6,25 @@ const defineConfig = (namespace, autoloadNs) => {
     i18n: {
       defaultLocale: "en-GB",
       locales: ["en-GB", "ms-MY"],
-      backend: {
-        loadPath: `${process.env.NEXT_PUBLIC_API_URL}/i18n/?lang={{lng}}&filename={{ns}}`,
-        customHeaders: {
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_AUTHORIZATION_TOKEN}`,
-        },
-        crossDomain: true,
-        allowMultiLoading: true,
-      },
+      // backend: {
+      //   loadPath: `${process.env.NEXT_PUBLIC_API_URL}/i18n/?lang={{lng}}&filename={{ns}}`,
+      //   customHeaders: {
+      //     Authorization: `Bearer ${process.env.NEXT_PUBLIC_AUTHORIZATION_TOKEN}`,
+      //   },
+      //   crossDomain: true,
+      //   allowMultiLoading: true,
+      // },
     },
+    localePath: "./public/locales",
     // debug: true,
     ns: namespace,
     autoloadNs: autoloadNs,
     load: "currentOnly",
     preload: ["en-GB", "ms-MY"],
-    serializeConfig: false,
+    // serializeConfig: false,
+    serializeConfig: true,
     reloadOnPrerender: true,
-    use: [I18NextHttpBackend],
+    // use: [I18NextHttpBackend],
   };
 };
 
