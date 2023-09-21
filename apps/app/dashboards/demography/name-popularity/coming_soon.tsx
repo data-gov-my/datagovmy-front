1a1a01614b7ff1a320ab409303db0d9ce5235629
leneherng <49067812+leneherng@users.noreply.github.com>
2023-09-21 14:32:09 +0800

    fix: dashboard live date (#314)

--- apps/app/dashboards/demography/name-popularity/coming_soon.tsx
+++ apps/app/dashboards/demography/name-popularity/coming_soon.tsx
@@ -5,11 +5,11 @@ import { DateTime } from "luxon";
 
 const ComingSoon: FunctionComponent = () => {
   const { t, i18n } = useTranslation(["dashboard-name-popularity"]);
-  const event = new Date(Date.UTC(2023, 8, 27, 0, 0, 0));
+  const event = new Date(Date.UTC(2023, 9, 12, 0, 0, 0));
 
   return (
     <div className="flex h-[400px] items-center justify-center">
-      <div className="text-dim mx-auto flex max-w-prose flex-col gap-y-6 rounded-xl border px-3 py-6 sm:px-9">
+      <div className="text-dim dark:border-washed-dark mx-auto flex max-w-prose flex-col gap-y-6 rounded-xl border px-3 py-6 sm:px-9">
         <h4 className="mx-auto">{t("coming_soon.index")}</h4>
         <span>
           <span className="text-primary dark:text-primary-dark">{t("header")}</span>
